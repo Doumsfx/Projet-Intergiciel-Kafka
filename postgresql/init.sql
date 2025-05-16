b6a97d54201c7237fe87422e2a65c2e96303565d
Alexis Pagnon <alexis.pagnon@uphf.fr>
2025-05-16 10:34:01 +0200

    sql

---
+++ postgresql/init.sql
@@ -0,0 +1,11 @@
+create table if not exists Client (
+	nom VARCHAR(50) primary key unique,
+	heure TIMESTAMP not null 
+);
+
+create table if not exists Logs (
+	id_log SERIAL primary key,
+	contenu TEXT not null,
+	heure TIMESTAMP not null 
+);
+
