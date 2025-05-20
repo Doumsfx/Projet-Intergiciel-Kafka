f3548344c16ac2f39507f5052fd28dd009a8f877
alexis-pagnon <alexpagnon2003@gmail.com>
2025-05-20 09:31:01 +0200

    test fonctions db

--- client-cons-db/src/main/java/insa/uphf/fr/dbClient/Main.java
+++ client-cons-db/src/main/java/insa/uphf/fr/dbClient/Main.java
@@ -6,6 +6,24 @@ public class Main {
     public static void main(String[] args){
         try (var connection =  DB.connect()){
             System.out.println("Connected to the PostgreSQL database.");
+
+            DB.insert_client("Michel");
+            DB.insert_client("Jean");
+            System.out.println(DB.is_connected("Michel"));
+
+            DB.insert_log("Test log");
+            DB.insert_log("Test log 2");
+            
+            System.out.println(DB.get_connected_clients());
+
+            DB.delete_client("Michel");
+            DB.is_connected("Michel");
+
+            DB.disconnect();
+            System.out.println("Disconnected from the PostgreSQL database.");
+
+
+
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
